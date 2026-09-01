751ab93eb8a049423d97f95f8eff0ef7d8dea790
Mohammad Mustafa Ahmad <yamiraj.dev@gmail.com>
2026-09-01 03:59:58 +0000

    Restore official logo in hero component

--- src/components/HeroWordmark.jsx
+++ src/components/HeroWordmark.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import TransparentHeroLogo from "@/components/TransparentHeroLogo";
+import { Image } from "@/components/ui/image";
+
+const LOGO_URL = "https://media.base44.com/images/public/6a905eb7064e9f37b1446f75/5e7f6fcc3_IMG_5309.png";
 
 export default function HeroWordmark({ mounted = false }) {
   return (
@@ -9,9 +11,14 @@ export default function HeroWordmark({ mounted = false }) {
       }`}
       style={{ transitionDelay: "300ms" }}
     >
-      <div className="relative w-[240px] sm:w-[300px] md:w-[340px] lg:w-[380px]">
-        <TransparentHeroLogo />
-      </div>
+      <Image
+        src={LOGO_URL}
+        alt="Changaroth Chambers — Brunei Darussalam"
+        fittingType="fit"
+        originWidth={1563}
+        originHeight={1563}
+        className="relative w-[240px] sm:w-[300px] md:w-[340px] lg:w-[380px]"
+      />
     </div>
   );
 }
